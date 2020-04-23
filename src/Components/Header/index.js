9ece6dedf5acea9925cfb47612dc250c605ce349
Christian <43688000+christianromarserad@users.noreply.github.com>
2020-04-23 15:07:11 -0400

    Header component responsive

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import styled, { withTheme } from 'styled-components';
+import { motion } from 'framer-motion';
 import { Flex, Text, InlineBlock, Button } from '../../StyledComponents'
 
 const SvgButton = styled(Button)`
     fill: white;
-    &:hover {
-        fill: black;
+`
+const UserInfo = styled(Flex)`
+    @media only screen and (max-width: 1000px) {
+        & {
+            width: 100%;
+            padding: 1rem;
+        }
+    }
+`
+
+const Links = styled(Flex)`
+    @media only screen and (max-width: 1000px) {
+        & {
+            width: 100%;
+            align-items: start;
+            padding: 1rem;
+        }
+    }
+`
+
+const HeaderBlock = styled(Flex)`
+    @media only screen and (max-width: 1000px) {
+        & {
+            flex-wrap: wrap;
+        }
     }
 `
 
@@ -15,23 +39,28 @@ function Header({ theme, header: { personName, profession, message, links } }) {
     }
 
     return (
-        <Flex h="400px">
-            <Flex w="50%" vertical p="8rem">
+        <HeaderBlock>
+            <UserInfo w="100%" vertical p="8rem">
                 <Text fs="3.5rem" ff={theme.fontFamilyHeading}>{personName}</Text>
-                <Text fs="1.6rem">{profession}</Text>
-                <Text>{message}</Text>
-            </Flex>
+                <Text fs="1.6rem" mb="1.6rem">{profession}</Text>
+                <Text w="80%">{message}</Text>
+            </UserInfo>
 
-            <Flex w="50%" vcenter p="3rem">
+            <Links w="100%" vcenter p="3rem">
                 {
                     links.map((link) => (
                         <SvgButton w="2.5rem" mr="2rem" onClick={() => openUrl(link.url)}>
-                            {link.svg}
+                            <motion.div
+                                whileHover={{
+                                    scale: 1.3,
+                                }}>
+                                {link.svg}
+                            </motion.div>
                         </SvgButton>
                     ))
                 }
-            </Flex>
-        </Flex>
+            </Links>
+        </HeaderBlock>
     );
 }
 
