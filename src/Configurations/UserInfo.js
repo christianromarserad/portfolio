5e78ba8125805e1b4be7563041f97c255f451380
Christian <43688000+christianromarserad@users.noreply.github.com>
2020-04-24 12:52:20 -0400

    Fixed user information

--- src/Configurations/UserInfo.js
+++ src/Configurations/UserInfo.js
@@ -4,7 +4,7 @@ const userInfo = {
     header: {
         personName: "Christian Serad",
         profession: "Software Developer",
-        message: "Hi, I am a new grad software development student. I love developing simple and intuitive web applications using React, JavaScript, and C#",
+        message: "Hi, I am a new grad software developer. I love developing simple and intuitive web applications using React, JavaScript, and C#",
         links: [
             {
                 name: "Github",
@@ -31,7 +31,7 @@ const userInfo = {
     project: [
         {
             name: "Big Calorie",
-            description: "A simple web application for planning and managing your meals each day. It can track food nutrients, and calculate your BMR.",
+            description: "A simple web application for planning and managing your meals. It can track food nutrients, and calculate your BMR.",
             videoUrl: "https://res.cloudinary.com/dnsrdfw3w/video/upload/w_600/v1587652762/bigcalorie_u5ubbb.mp4",
             technologies: [
                 {
@@ -59,19 +59,19 @@ const userInfo = {
                 {
                     name: "Website",
                     svg: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M7.859 14.691l-.81.805a1.814 1.814 0 01-2.545 0 1.762 1.762 0 010-2.504l2.98-2.955c.617-.613 1.779-1.515 2.626-.675a.992.992 0 101.397-1.407c-1.438-1.428-3.566-1.164-5.419.675l-2.98 2.956A3.719 3.719 0 002 14.244a3.72 3.72 0 001.108 2.658 3.779 3.779 0 002.669 1.096c.967 0 1.934-.365 2.669-1.096l.811-.805a.988.988 0 00.005-1.4.995.995 0 00-1.403-.006zm9.032-11.484c-1.547-1.534-3.709-1.617-5.139-.197l-1.009 1.002a.99.99 0 101.396 1.406l1.01-1.001c.74-.736 1.711-.431 2.346.197.336.335.522.779.522 1.252s-.186.917-.522 1.251l-3.18 3.154c-1.454 1.441-2.136.766-2.427.477a.99.99 0 10-1.396 1.406c.668.662 1.43.99 2.228.99.977 0 2.01-.492 2.993-1.467l3.18-3.153A3.732 3.732 0 0018 5.866a3.726 3.726 0 00-1.109-2.659z" /></svg>,
-                    url: "https://www.youtube.com/watch?v=EKetdJxTkq0&amp;feature=youtu.be"
+                    url: "https://bigcalorie.herokuapp.com/"
                 },
                 {
                     name: "Github",
                     svg: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10.015 9.949h-.03c-1.191 0-2.24-.303-2.861.268a1.57 1.57 0 00-.527 1.197c0 1.852 1.483 2.08 3.389 2.08h.029c1.905 0 3.389-.229 3.389-2.08 0-.443-.156-.856-.527-1.197-.622-.571-1.671-.268-2.862-.268zM8.393 12.48c-.363 0-.656-.408-.656-.91s.293-.908.656-.908c.363 0 .657.406.657.908.001.502-.293.91-.657.91zm3.213 0c-.363 0-.657-.408-.657-.91s.294-.908.657-.908c.362 0 .656.406.656.908.001.502-.293.91-.656.91zM10 .4C4.698.4.4 4.698.4 10s4.298 9.6 9.6 9.6 9.6-4.298 9.6-9.6S15.302.4 10 .4zm.876 13.539c-.172 0-.514 0-.876.002-.362-.002-.704-.002-.876-.002-.76 0-3.772-.059-3.772-3.689 0-.834.286-1.445.755-1.955-.074-.184-.078-1.232.32-2.236 0 0 .916.1 2.301 1.051.289-.081.781-.122 1.272-.122s.982.041 1.273.121c1.385-.951 2.301-1.051 2.301-1.051.398 1.004.395 2.053.32 2.236.469.51.755 1.121.755 1.955-.001 3.632-3.013 3.69-3.773 3.69z" /></svg>,
-                    url: "https://www.youtube.com/watch?v=EKetdJxTkq0&amp;feature=youtu.be"
+                    url: "https://github.com/christianromarserad/big-calorie"
                 }
             ]
         },
         {
             name: "School Notebook",
             videoUrl: "https://res.cloudinary.com/dnsrdfw3w/video/upload/w_600/v1587652755/schoolnotebook_q7d170.mp4",
-            description: "A note taking web application for students which they can easily share notes to each other. The sharing capability allows users for collaboration.",
+            description: "A note taking web application for students which they can easily share notes to each other. The sharing capability allows them to collaborate.",
             technologies: [
                 {
                     name: "React",
@@ -119,7 +119,7 @@ const userInfo = {
         {
             name: "Acute Pediatrics Orientation",
             videoUrl: "https://res.cloudinary.com/dnsrdfw3w/video/upload/w_600/v1587652762/acutepediatric_ak4dqd.mp4",
-            description: "A web application that used for employees for online training and orientation. It is mainly intented for the McMaster children hospital employees.",
+            description: "A web application that is used by employees for online training and orientation. It was intented for the McMaster Children Hospital employees.",
             technologies: [
                 {
                     name: "SQL Server",
@@ -159,7 +159,7 @@ const userInfo = {
         {
             name: "Hamilton Spray Pads",
             videoUrl: "https://res.cloudinary.com/dnsrdfw3w/video/upload/w_600/v1587653848/hamiltonspraypads_aycn95.mp4",
-            description: "A web application that locates spray pads in Hamilton, ON. You can search for specific and nearest spray pad locations",
+            description: "A web application that locates spray pads in Hamilton, ON. You can search for specific and nearest spray pad",
             technologies: [
                 {
                     name: "jQuery",
