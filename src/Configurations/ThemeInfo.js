bec13974757243a44ea0b183eea255b64121c7e3
Christian <43688000+christianromarserad@users.noreply.github.com>
2020-04-24 09:23:31 -0400

    Separate the user and theme info data

---
+++ src/Configurations/ThemeInfo.js
@@ -0,0 +1,10 @@
+
+const themeInfo = {
+    backgroundColor: "#303030",
+    textColor: "#fff",
+    cardColor: "#424242",
+    fontFamilyHeading: "'Luckiest Guy', sans-serif;",
+    fontFamily: "Roboto, sans-serif"
+};
+
+export default themeInfo;
